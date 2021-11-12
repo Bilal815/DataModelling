bfa9679d8abe3bbd110401c8573f7394d285d157
Bilal Ahmed <62904483+Bilal815@users.noreply.github.com>
2021-11-12 18:59:28 +0500

    Update etl_job.py

--- etl_job.py
+++ etl_job.py
@@ -1,15 +1,17 @@
 # import etl_scripts
 from etl_scripts import *
 
+
 # enter database details
 user = 'root'  # please write your user name
-password = 'password'  # please write your password
-host = 'localhost'  # please write your host address
+password = 'root'  # please write your password
+host = '127.0.0.1'  # please write your host address
 port = 3306
 # use 'northwind_schema', if you're working with northwind_project.
 database = 'store_schema'
-if __name__ == '__main__':
 
+
+if __name__ == '__main__':
     # specifying the zip file name and zip file extract path
     # use 'northwind_project/Data.zip', if you're working with northwind_project.
     zip_name = 'store_project/Data.zip'
@@ -31,19 +33,18 @@ if __name__ == '__main__':
     '''
 
     # store_schema
-    sql_table = ["products", "shippers", "customers", "order_statuses",
-                 "orders", "order_items"]
+    store_schema = ["customers", "order_items", "order_statuses", "orders", "products", "shippers"]
 
-    # northwind_schmea
+    # northwind_schema
     '''
-    In the below sql table, I have purposley changed the order of the table name. 
+    In the below sql table, I have purposely changed the order of the table name. 
     It's a challenge to correct the table name order. 
     Above, I've described the way how to write these table names in correct order
     '''
     # sql_table = [ "suppliers", "employees",
     #             "products", "categories","customers","orders"]
 
-    for table in sql_table:
+    for table in store_schema:
         # path, where extracted data from zip is located
         # use 'northwind_project/Data/', if you're working with northwind_project.
         path = 'store_project/Data/'
